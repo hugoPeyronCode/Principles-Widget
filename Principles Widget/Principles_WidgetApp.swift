a66463674d15a68cb1bf59d481f05d6450a880a2
Hugo Peyron <hugopeyron@gmail.com>
2024-06-14 10:19:07 +0200

    Initial Commit

---
+++ Principles Widget/Principles_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Principles_WidgetApp.swift
+//  Principles Widget
+//
+//  Created by Hugo Peyron on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Principles_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
